f661f18300e00cfdfe35d718d1ea8145c2938ec3
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 20:20:59 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace couponcoded6
+{
+    internal class program
+    {
+        public class CoupenCode
+        {
+            public static void main(String[] args)
+            {
+                Console.WriteLine("generate random coupon");
+            }
+        }
+    }
+}
