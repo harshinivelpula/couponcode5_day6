348860ffa04504663537b4fb1b0a5bf6b3e85d3e
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 20:21:36 +0530

    generate random coupon

---
+++ coupon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace couponcoded6
+{
+    internal class coupon
+    {
+        public class CoupenCode
+        {
+            public static void main(String[] args)
+            {
+                char[] chars = "abcdefgh".ToCharArray();
+                int max = 100000000;
+                double d = Math.random();
+                int random = (int)(d * max);
+                String sb = "";
+                Console.WriteLine("random double " + d + " random at start " + random);
+
+                while (random > 0)
+                {
+                    sb = sb + (chars[random % chars.Length]);
+                    random /= chars.Length;
+                    Console.WriteLine(random + "  " + random % chars.Length + " sb is " + sb);
+                }
+                String couponCode = sb.ToString();
+                Console.WriteLine("Coupon Code: " + couponCode);
+            }
+        }
+    }
+}
